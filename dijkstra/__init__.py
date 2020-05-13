46fe106e08d89f4fc87f523c621f9529679e8dbd
Jukka Aho <ahojukka5@gmail.com>
2020-05-13 09:41:45 +0700

    Add Dijkstra to public API

--- dijkstra/__init__.py
+++ dijkstra/__init__.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from .graph import Graph
+from .dijkstra import Dijkstra
 
 __version__ = "0.1.0"
 __author__ = "Jukka Aho <ahojukka5@gmail.com>"
-__all__ = [Graph]
+__all__ = [Graph, Dijkstra]
