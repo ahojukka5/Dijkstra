ce32b25c1df9e1c45037a04a8909f6486c1e7137
Jukka Aho <ahojukka5@gmail.com>
2020-05-17 21:16:24 +0700

    Update version to 0.2.1

--- dijkstra/__init__.py
+++ dijkstra/__init__.py
@@ -4,6 +4,6 @@
 from .graph import Graph, generate_random_graph
 from .dijkstra import DijkstraSPF
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Jukka Aho <ahojukka5@gmail.com>"
 __all__ = [Graph, DijkstraSPF, generate_random_graph]
